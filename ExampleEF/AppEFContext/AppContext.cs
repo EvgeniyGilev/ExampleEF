01b7ebe085a817d846760310f5cc5a6d9de04799
Евгений Гилев <euggil@yandex.ru>
2022-04-04 13:12:21 +0500

    Добавьте файлы проекта.

---
+++ ExampleEF/AppEFContext/AppContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExampleEF.EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExampleEF.AppEFContext
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = HOME; Initial Catalog = testing; Persist Security Info=True;User ID = user; Password=Rfgecnf!;TrustServerCertificate=True;");
+        }
+    }
+}
